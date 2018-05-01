eae54d478b7b2617bac54f40b392194fc07a37f6
Dariusz Rosiek <daro.krk@interia.pl>
2018-05-01 16:11:18 +0200

    Add minus button click test

--- src/components/Player/Player.test.js
+++ src/components/Player/Player.test.js
@@ -31,3 +31,13 @@ it('should call onPlayerScoreChange with 1 when plus button is clicked', () => {
 
     expect(mockedOnPlayerScoreChange).toBeCalledWith(1);
 });
+it('should call onPlayerScoreChange with -1 when minus button is clicked', () => {
+    const mockedOnPlayerScoreChange = jest.fn();
+    const playerComponent = shallow(<Player onPlayerScoreChange={mockedOnPlayerScoreChange} />);
+
+    const minusButton = playerComponent.find('.Player__button').at(1);
+
+    minusButton.simulate('click');
+
+    expect(mockedOnPlayerScoreChange).toBeCalledWith(-1);
+});
